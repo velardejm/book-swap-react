50e3d370a67a84adbf87c14d3f136b788b64ee5b
JM Velarde <jmvelarde@JMs-MacBook-Pro.local>
2023-12-29 12:16:55 +0300

    Try react app connection to node.js server.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Listening to requests from port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    console.log('A request is received from React.');
+    res.json({
+        response: 'response data'
+    });
+});
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
 import Header from './Header';
 import Hero from './Hero';
 
 export default function Home() {
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch('http://localhost:3001/');
+      const jsonResponse = await res.json();
+      console.log(jsonResponse);
+    };
+
+    getData();
+
+    // fetch('http://localhost:3001/')
+    //   .then((res) => {
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  });
+
   return (
     <>
       <Header />
