3377b0a24ecc1456d3d3efddb115f1d3483bb82e
JM Velarde <jmvelarde@JMs-MacBook-Pro.local>
2024-01-01 22:05:21 +0300

    Disable log in when user still logged in.

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -38,28 +38,34 @@ export default function LogIn() {
 
   return (
     <div className="flex flex-col items-center bg-blue-200 mx-5 px-5 pb-10 mt-10">
-      <h1 className="text-3xl font-bold py-10 text-center">Log In</h1>
-      <form method="post" className="flex flex-col" onSubmit={handleSubmit}>
-        <FormInput
-          label="Username:"
-          type="text"
-          name="username"
-          onChangeHandler={handleChange}
-          autofocus={true}
-        />
-        <FormInput
-          label="Password:"
-          type="password"
-          name="password"
-          onChangeHandler={handleChange}
-        />
-        <button
-          className={`btn btn-primary w-28 self-center mt-2`}
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
+      {localStorage.getItem('token') ? (
+        <p className='font-bold text-2xl text-center px-5 my-20'>You are already logged in.</p>
+      ) : (
+        <>
+          <h1 className="text-3xl font-bold py-10 text-center">Log In</h1>
+          <form method="post" className="flex flex-col" onSubmit={handleSubmit}>
+            <FormInput
+              label="Username:"
+              type="text"
+              name="username"
+              onChangeHandler={handleChange}
+              autofocus={true}
+            />
+            <FormInput
+              label="Password:"
+              type="password"
+              name="password"
+              onChangeHandler={handleChange}
+            />
+            <button
+              className={`btn btn-primary w-28 self-center mt-2`}
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
