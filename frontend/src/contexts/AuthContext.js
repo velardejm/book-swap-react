c2b17b404ac0c30e0ce70052327be6d2d6f766ce
velardejm <114612562+velardejm@users.noreply.github.com>
2024-01-15 15:07:36 +0300

    Update AuthContext.js

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -9,16 +9,22 @@ export const AuthContextProvider = ({children}) => {
 
     useEffect(() => {
         if(localStorage.getItem('token')) {
+           //Verify token (add verification route that returns username.). Use hook?
+            //set user using the data returned from the above auth route.
             setIsLoggedIn(true);
         }
-    });
+    }); //add isLoggedIn dependency.
 
+      //add parameter for formData
     const logIn = () => {
+        //login using formdata. it seems loginhook is good to use here.
+      //or, put the full login logice here and reuse the authcontext (instead of using authcontext and uselogin as before.)
         setIsLoggedIn(true);
     }
 
     const logOut = () => {
         localStorage.removeItem('token');
+        //setUser to null
         setIsLoggedIn(false);
     }
 
