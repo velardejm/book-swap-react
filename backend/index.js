d9847a96f1d134ea8cc51fdf539ce0c7f2853121
JM Velarde <velarde.jmv8@gmail.com>
2024-01-18 21:47:18 +0300

    working...

--- backend/index.js
+++ backend/index.js
@@ -9,78 +9,6 @@ const authenticationRoutes = require("./routes/authentication");
 const app = express();
 const port = 3001;
 
-// let users = [
-//   {
-//     username: "booklover1",
-//     password: "$2b$10$/35QC92KG2EO3TsOIgOqaOYZh3gce9SyHOIK9SM79YG8VfrrH7ykq",
-//   },
-//   {
-//     username: "readingfanatic",
-//     password: "$2b$10$/35QC92KG2EO3TsOIgOqaOYZh3gce9SyHOIK9SM79YG8VfrrH7ykq",
-//   },
-// ];
-
-// let usersData = [
-//   {
-//     name: "User 1",
-//     username: "booklover1",
-//     email: "booklover1@example.com",
-//     booksAvailable: [
-//       {
-//         bookId: "1a",
-//         title: "The Catcher in the Rye",
-//         author: "J.D. Salinger",
-//         genre: "Fiction",
-//         condition: "Like New",
-//         inTransaction: false,
-//       },
-//       {
-//         bookId: "1b",
-//         title: "The Hobbit",
-//         author: "J.R.R. Tolkien",
-//         genre: "Fantasy",
-//         condition: "Good",
-//         inTransaction: false,
-//       },
-//     ],
-//   },
-//   {
-//     name: "User 2",
-//     username: "readingfanatic",
-//     email: "readingfanatic@example.com",
-//     booksAvailable: [
-//       {
-//         bookId: "2a",
-//         title: "To Kill a Mockingbird",
-//         author: "Harper Lee",
-//         genre: "Fiction",
-//         condition: "Very Good",
-//         inTransaction: false,
-//       },
-//       {
-//         bookId: "2b",
-//         title: "1984",
-//         author: "George Orwell",
-//         genre: "Dystopian",
-//         condition: "Fair",
-//         inTransaction: false,
-//       },
-//     ],
-//   },
-// ];
-
-// let usersTransactionData = [
-//   {
-//     username: "booklover1",
-//     incomingSwapRequests: [],
-//     // {username: 'requestor's username', offer: {book: book, amount: amount}}
-//   },
-//   {
-//     username: "readingfanatic",
-//     incomingSwapRequests: [],
-//   },
-// ];
-
 let users;
 let usersData;
 let usersTransactionData;
@@ -116,7 +44,7 @@ function authenticateToken(req, res, next) {
 
 app.use("/", authenticationRoutes);
 
-// app.get("/", (req, res) => {});l
+// app.get("/", (req, res) => {});
 
 app.get("/listings", (req, res) => {
   const bookListings = usersData.map((user) => {
@@ -161,75 +89,11 @@ app.get("/dashboard", authenticateToken, (req, res) => {
 });
 
 app.get("/authenticate", authenticateToken, (req, res) => {
-  // res.status(200).json({ user: req.user.username });
   const user = usersData.find((u) => u.username === req.user.username);
   if (user) {
     res.status(200).json({ data: user });
   }
 });
-
-// app.get("/get-user", authenticateToken, (req, res) => {
-//   const user = usersData.find((u) => u.username === req.user.username);
-//   if (user) {
-//     res.status(200).json({ data: user });
-//   }
-// });
-
-app.get("/swap/:owner/:bookId", authenticateToken, (req, res) => {
-  const user = usersData.find((user) => user.username === req.params.owner);
-  const book = user.booksAvailable.find(
-    (book) => book.bookId === req.params.bookId
-  );
-  res.status(200).json({ data: book });
-});
-
-// app.post("/signup", async (req, res) => {
-//   const { name, email, username, password } = req.body;
-//   const hashedPassword = await bcrypt.hash(password, 10);
-
-//   const newUser = {
-//     username: username,
-//     password: hashedPassword,
-//   };
-
-//   const newUserData = {
-//     name: name,
-//     email: email,
-//     username: username,
-//     booksAvailable: [],
-//   };
-
-//   const foundExistingUser = !!users.find((user) => user.username === username);
-//   const foundExistingEmail = !!usersData.find((user) => user.email === email);
-
-//   if (foundExistingUser || foundExistingEmail) {
-//     return res
-//       .status(409)
-//       .json({ message: "username or e-mail already exists" });
-//   }
-//   users.push(newUser);
-//   usersData.push(newUserData);
-//   res.json({
-//     message: "Registration successful!",
-//   });
-// });
-
-// app.post("/login", async (req, res) => {
-//   const { username, password } = req.body;
-//   const user = users.find((u) => u.username === username);
-//   if (!user) {
-//     res.status(401).json({ message: "User not found." });
-//   } else {
-//     if (await bcrypt.compare(password, user.password)) {
-//       const token = jwt.sign({ username: user.username }, "SECRET", {
-//         expiresIn: 3600,
-//       });
-//       res.status(200).json({ user: user.username, token: token });
-//     } else {
-//       res.status(401).json({ message: "Incorrect password." });
-//     }
-//   }
-// });
 
 app.post("/book", authenticateToken, (req, res) => {
   const userData = usersData.find(
@@ -239,9 +103,16 @@ app.post("/book", authenticateToken, (req, res) => {
     userData.booksAvailable.push(req.body);
     res.status(200).json({
       message: "New book added successfully.",
-      // data: userData
     });
   }
+});
+
+app.get("/swap/:owner/:bookId", authenticateToken, (req, res) => {
+  const user = usersData.find((user) => user.username === req.params.owner);
+  const book = user.booksAvailable.find(
+    (book) => book.bookId === req.params.bookId
+  );
+  res.status(200).json({ data: book });
 });
 
 app.post("/swap/:owner/:bookId/:user", authenticateToken, (req, res) => {
