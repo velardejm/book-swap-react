78d0fe9540064637f23de0b6eba773aa2228ffd0
JM Velarde <velarde.jmv8@gmail.com>
2024-01-26 22:02:07 +0300

    Updating IncomingTransaction component.

--- backend/routes/swap.js
+++ backend/routes/swap.js
@@ -39,8 +39,13 @@ swapRouter.post("/:user/:bookId/:owner", authenticateToken, (req, res) => {
     (user) => user.userId === bookOwnerId
   );
 
+  const { sentRequests } = usersTransactionData.find(
+    (user) => user.userId === requestorId
+  );
+
   const request = {
     requestId: Math.floor(Math.random() * 1000000000).toString(),
+    bookOwnerId: bookOwnerId,
     requestedBookId: requestedBookId,
     requestorId: requestorId,
     bookToSwapId: bookToSwapId,
@@ -53,21 +58,80 @@ swapRouter.post("/:user/:bookId/:owner", authenticateToken, (req, res) => {
     return requestedBookId + requestorId === requestCheckString;
   });
 
-  console.log(requestIndex);
-
   if (requestIndex === -1) {
-    incomingRequests.push({
+    requestedBook.inTransaction = true;
+    bookToSwap.inTransaction = true;
+
+    const requestData = {
       ...request,
       requestor,
       bookToSwap,
       requestedBook,
       status: "reviewByOwner",
-    });
+    };
+
+    incomingRequests.push(requestData);
+    sentRequests.push(requestData);
+
     saveData(data);
     res.status(200).json({ message: "Request sent successfully." });
   } else {
     res.status(404).json({ message: "Request already exists." });
   }
+});
+
+swapRouter.post("/respond/:transactionId", authenticateToken, (req, res) => {
+  // steps
+  // 1. Find index
+  // 2. If accepted, swap books
+  // 3. If rejected, move transaction to closed.
+
+  const transactionData = usersTransactionData.find(
+    (data) => data.userId === req.user.userId
+  );
+  const { incomingRequests, transactionsToConfirm } = transactionData;
+  const requestIndex = incomingRequests.findIndex(
+    (request) => request.requestId === req.body.requestId
+  );
+  const respondedRequest = incomingRequests.splice(requestIndex, 1);
+  const { requestorId, bookToSwapId, bookOwnerId, requestedBookId } =
+    respondedRequest[0];
+
+  // SWAP BOOK
+  const { booksAvailable } = usersData.find(
+    (user) => user.userId === requestorId
+  );
+  const bookToSwapIndex = booksAvailable.findIndex(
+    (book) => book.bookId === bookToSwapId
+  );
+  const requestorBook = booksAvailable.splice(bookToSwapIndex, 1);
+
+  const { booksAvailable: ownerBooksAvailable } = usersData.find(
+    (user) => user.userId === bookOwnerId
+  );
+  const requestedBookBookIndex = ownerBooksAvailable.findIndex(
+    (book) => book.bookId === requestedBookId
+  );
+  const requestedBook = booksAvailable.splice(requestedBookBookIndex, 1);
+
+  booksAvailable.push(requestedBook);
+  ownerBooksAvailable.push(requestorBook);
+
+  // MOVE TRANSACTION TO TRANSACTIONS TO CONFIRM
+
+  transactionsToConfirm.push(respondedRequest[0]);
+  const {
+    sentRequests,
+    transactionsToConfirm: requestorTransactionsToConfirm,
+  } = usersTransactionData.find((user) => user.userId === requestorId);
+  const sentRequestIndex = sentRequests.findIndex(
+    (request) => request.requestId === req.body.requestId
+  );
+  const sentRequest = sentRequests.splice(sentRequestIndex, 1);
+  requestorTransactionsToConfirm.push(sentRequest);
+
+  saveData(data);
+  res.status(200).json({ data: incomingRequests });
 });
 
 module.exports = swapRouter;
--- frontend/src/components/Transactions/IncomingRequests.js
+++ frontend/src/components/Transactions/IncomingRequests.js
@@ -6,6 +6,25 @@ export default function IncomingRequests() {
     'http://localhost:3001/users/transactions'
   );
 
+  const respondToRequest = async (response, requestId) => {
+    const res = await fetch(`http://localhost:3001/swap/respond/${requestId}`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        response: response,
+        requestId: requestId,
+      }),
+    });
+
+    if (res.status === 200) {
+      const { data } = await res.json();
+      setIncomingRequests(data);
+    }
+  };
+
   if (!incomingRequests) return null;
 
   return (
@@ -25,10 +44,18 @@ export default function IncomingRequests() {
               </div>
             </div>
             <div className="flex justify-center">
-              <Link className="text-green-800 mr-5" to="#">
+              <Link
+                onClick={() => respondToRequest('accept', requestId)}
+                className="text-green-800 mr-5"
+                to="#"
+              >
                 Accept Offer
               </Link>
-              <Link className="text-red-800 ml-5" to="#">
+              <Link
+                onClick={() => respondToRequest('reject', requestId)}
+                className="text-red-800 ml-5"
+                to="#"
+              >
                 Reject Offer
               </Link>
             </div>
